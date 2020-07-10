a044695666d69cc837af2b35e04e06da3ac943b4
enfipy <enfipy@gmail.com>
2020-07-10 18:58:01 +0300

    Add env for ABCI server url

--- substrate/pallets/abci-direct/src/request.rs
+++ substrate/pallets/abci-direct/src/request.rs
@@ -5,10 +5,20 @@ use std::time::Duration;
 
 pub const ABCI_SERVER_URL: &str = "http://localhost:8082/abci/v1/";
 
+fn get_server_url() -> String {
+    match std::env::var("ABCI_SERVER_URL") {
+        Ok(val) => val,
+        Err(_) => ABCI_SERVER_URL.to_owned(),
+    }
+}
+
 pub fn get_method(method_name: &str) -> core::result::Result<(), &'static str> {
-    let request_url = format!("{}{}", ABCI_SERVER_URL, method_name);
-    let res = isahc::get(&request_url)
-        .map_err(|_| "Failed to send request")?;
+    let request_url = format!("{}{}", &get_server_url(), method_name);
+    let res = Request::get(&request_url)
+        .header("Content-Type", "application/json")
+        .timeout(Duration::from_secs(1))
+        .body("").map_err(|_| "Failed to add body to request")?
+        .send().map_err(|_| "Failed to send request")?;
     let status = res.status().as_u16();
     if status != 200 {
         Err("Unexpected HTTP request status code")
@@ -21,7 +31,7 @@ pub fn post_method<T: Serialize>(
     method_name: &str,
     msg: &T,
 ) -> core::result::Result<(), &'static str> {
-    let request_url = format!("{}{}", ABCI_SERVER_URL, method_name);
+    let request_url = format!("{}{}", &get_server_url(), method_name);
     let val = serde_json::to_string(&msg).map_err(|_| "Failed to convert msg to json string")?;
     let res = Request::post(&request_url)
         .header("Content-Type", "application/json")
