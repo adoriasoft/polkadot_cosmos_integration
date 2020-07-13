dca3848c26f3de49329857ff7793173eb64a44c3
Mr-Leshiy <leshiy12345678@gmail.com>
2020-07-13 13:55:36 +0300

    update index.js

--- abci-grpc/src/example/example.go
+++ abci-grpc/src/example/example.go
@@ -9,13 +9,15 @@ import (
 func main() {
 	log.Print("Start sign")
 
-	private_key_encoded := "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQglWBgX4mTlp1BVfS3uQfNPF9xfi7PeKxC4XLk1WU080GhRANCAARF09Qag4BNEEET4LYf3Q3w12k9AnoArBhB2cpZ1F3IqOZyFpbEOr63W2kbPz97p7OlFLAwqILHxsJvnHkqVQYy"
+	//bob_private_key_encoded := "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgYGBgX72QndBBmdUlAJYi1Da/p3Njvybb/NJr7zMKkjGhRANCAASEUS2ykO7Fwf1U/Db6IzfWqgLIGjP1R/Uu4UAMEmWBar/26bUe7i0x6K8EIsAamPyu8pRpAIe9JmdKp2cAdZeb"
 
-	message := "Alice" + "Bob" + strconv.Itoa(97)
+	alice_private_key_encoded := "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQglWBgX4mTlp1BVfS3uQfNPF9xfi7PeKxC4XLk1WU080GhRANCAARF09Qag4BNEEET4LYf3Q3w12k9AnoArBhB2cpZ1F3IqOZyFpbEOr63W2kbPz97p7OlFLAwqILHxsJvnHkqVQYy"
+
+	message := "Alice" + "Bob" + strconv.Itoa(5000)
 
 	log.Print(message)
 
-	signature, err := token.Sign(message, "seed2", private_key_encoded)
+	signature, err := token.Sign(message, "sign_seed", alice_private_key_encoded)
 
 	if err != nil {
 		log.Fatal(err)
