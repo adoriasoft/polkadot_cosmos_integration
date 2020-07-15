fb66a9f39fcf4234b27f4040c2d1c71f19b1c858
enfipy <enfipy@gmail.com>
2020-07-15 12:22:25 +0300

    Alter maintain docs and abci-direct tests

--- substrate/pallets/abci-direct/src/tests.rs
+++ substrate/pallets/abci-direct/src/tests.rs
@@ -1,13 +1,27 @@
 // Tests to be written here
 
+use super::TxMessage;
 use crate::mock::*;
 use frame_support::assert_ok;
+use sp_runtime::transaction_validity::TransactionSource;
 
 #[test]
-fn block_on_initialize() {
+fn test_get_method() {
     // Should run with the working abci server
     new_test_ext().execute_with(|| {
-        //AbciModule::do_init_chain();
-        //assert_ok!(crate::request::get_method("InitChain"));
+        assert_ok!(crate::request::get_method("InitChain"));
+    });
+}
+
+#[test]
+fn test_call_deliver_tx() {
+    // Should run with the working abci server
+    new_test_ext().execute_with(|| {
+        assert_ok!(AbciModule::deliver_tx(
+            Origin::signed(Default::default()),
+            TxMessage { tx: vec![] },
+        ));
+        let source: TransactionSource = TransactionSource::InBlock;
+        AbciModule::do_check_tx(source, vec![]);
     });
 }
