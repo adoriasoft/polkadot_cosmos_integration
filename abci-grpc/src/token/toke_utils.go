03c4c739027dd86ef3b89f314b2e2c4b90c49ceb
Mr-Leshiy <leshiy12345678@gmail.com>
2020-07-16 11:31:31 +0300

    update logs

--- abci-grpc/src/token/toke_utils.go
+++ abci-grpc/src/token/toke_utils.go
@@ -12,9 +12,9 @@ func GenerateKeyPairForAccount(account_name string) (string, string) {
 	public_key_encoded := PKBase64Encode(public_key)
 	private_key_encoded := SKBase64Encode(private_key)
 
-	log.Printf("%v, private key: %v", account_name, private_key_encoded)
+	log.Printf("%v, private key (save it!!!!): %v", account_name, private_key_encoded)
 
-	log.Printf("{ \"AccountName\": \"%v\", \"PublicKey\": \"%v\"}", account_name, public_key_encoded)
+	log.Printf("Create new account message: { \"AccountName\": \"%v\", \"PublicKey\": \"%v\"}", account_name, public_key_encoded)
 
 	return private_key_encoded, public_key_encoded
 }
@@ -28,5 +28,5 @@ func GenerateTransactionMessage(from string, to string, amount int, private_key 
 		log.Fatal(err)
 	}
 
-	log.Printf("{ \"From\": \"%v\", \"To\": \"%v\", \"Amount\": %v, \"Signature\": \"%v\", \"Nonce\": %v}", from, to, amount, signature, nonce)
+	log.Printf("Transaction message: { \"From\": \"%v\", \"To\": \"%v\", \"Amount\": %v, \"Signature\": \"%v\", \"Nonce\": %v}", from, to, amount, signature, nonce)
 }
