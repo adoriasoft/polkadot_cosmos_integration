1b0ef21531f093b4619b9a9ca9b061cfca9046ac
enfipy <enfipy@gmail.com>
2020-07-24 19:57:20 +0300

    Alter abci and request

--- substrate/pallets/abci/build.rs
+++ substrate/pallets/abci/build.rs
@@ -1,3 +1,4 @@
 fn main() {
     tonic_build::compile_protos("proto/abci.proto").unwrap();
+    tonic_build::compile_protos("proto/types.proto").unwrap();
 }
--- substrate/pallets/abci/src/lib.rs
+++ substrate/pallets/abci/src/lib.rs
@@ -1,10 +1,16 @@
-use abci::abci_client::AbciClient;
-pub use abci::*;
+use abci_proto::abci_client::AbciClient;
+use abci_proto::EmptyMessage;
+use tendermint_proto::abci_application_client::AbciApplicationClient;
+use tendermint_proto::RequestEcho;
 use std::time::Duration;
 use tokio::runtime::Runtime;
 
-pub mod abci {
+pub mod abci_proto {
     tonic::include_proto!("abci");
+}
+
+pub mod tendermint_proto {
+    tonic::include_proto!("types");
 }
 
 pub fn send_test_method(abci_endpoint: String) -> Result<(), Box<dyn std::error::Error>> {
@@ -19,8 +25,34 @@ pub fn send_test_method(abci_endpoint: String) -> Result<(), Box<dyn std::error:
             .expect("failed to set timeout for future")
     })?;
 
+    let request = tonic::Request::new(EmptyMessage {});
+
+    let req = client.init_chain(request);
+    let response = rt.block_on(async move {
+        tokio::time::timeout(Duration::from_secs(1), req)
+            .await
+            .expect("failed to set timeout for future")
+    })?;
+
+    println!("RESPONSE={:?}", response);
+    Ok(())
+}
+
+pub fn send_tendermint_method(abci_endpoint: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut rt = Runtime::new()?;
+
+    // Get server URL from ENV variable and translate it into static str
+    let endpoint: &'static str = Box::leak(abci_endpoint.into_boxed_str());
+    let client = AbciApplicationClient::connect(endpoint);
+    let mut client = rt.block_on(async move {
+        tokio::time::timeout(Duration::from_secs(1), client)
+            .await
+            .expect("failed to set timeout for future")
+    })?;
+
     let request = tonic::Request::new(RequestEcho {
-        message: "Hello!".to_owned(),
+        // tx: "Hello".as_bytes().to_vec(),
+        message: "Hello".to_owned(),
     });
 
     let req = client.echo(request);
@@ -40,7 +72,16 @@ mod test {
 
     #[test]
     fn test_basic_logic() {
-        let result = send_test_method("tcp://127.0.0.1:26658".to_owned());
+        let result = send_test_method("http://0.0.0.0:8081".to_owned());
+        // let result = send_test_method("tcp://127.0.0.1:26658".to_owned());
+        println!("result: {:?}", result);
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn test_tendermint_req() {
+        // let result = send_test_method("http://0.0.0.0:8081".to_owned());
+        let result = send_tendermint_method("tcp://127.0.0.1:26658".to_owned());
         println!("result: {:?}", result);
         assert_eq!(result.is_ok(), true);
     }
