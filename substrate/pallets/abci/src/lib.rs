3f23da79d5a2840acaf35911d52288438e76e1e9
enfipy <enfipy@gmail.com>
2020-07-30 22:20:59 +0300

    Add working ABCI requests

--- substrate/pallets/abci/src/lib.rs
+++ substrate/pallets/abci/src/lib.rs
@@ -3,7 +3,7 @@ pub mod protos;
 use lazy_static::lazy_static;
 use owning_ref::MutexGuardRefMut;
 use protos::abci_application_client;
-use std::{sync::Mutex, time::Duration};
+use std::{sync::Mutex, time::{Duration, SystemTime}};
 use tokio::runtime::Runtime;
 
 lazy_static! {
@@ -89,12 +89,27 @@ impl Client {
     }
 
     pub fn init_chain(&mut self, chain_id: String, app_state_bytes: Vec<u8>) -> AbciResult<()> {
+        let now = SystemTime::now();
+
         let request = tonic::Request::new(protos::RequestInitChain {
+            time: Some(now.into()),
             chain_id: chain_id,
+            consensus_params: Some(protos::ConsensusParams {
+                block: Some(protos::BlockParams {
+                    max_bytes: 22020096,
+                    max_gas: -1,
+                }),
+                evidence: Some(protos::EvidenceParams {
+                    max_age_num_blocks: 100000,
+                    max_age_duration: Some(Duration::from_micros(172800000000000).into()),
+                }),
+                validator: Some(protos::ValidatorParams {
+                    pub_key_types: vec!["ed25519".to_owned()],
+                }),
+            }),
+            validators: vec![],
+            // protos::ValidatorUpdate { pub_key: Some(protos::PubKey { r#type: "ed25519".to_owned(), data: "".as_bytes().to_vec() }), power: 1 }
             app_state_bytes: app_state_bytes,
-            time: None,
-            validators: Vec::new(),
-            consensus_params: None,
         });
 
         let future = self.client.init_chain(request);
@@ -108,17 +123,29 @@ impl Client {
         Ok(())
     }
 
-    pub fn begin_block(&mut self, hash: Vec<u8>) -> AbciResult<()> {
-        let last_commit_info = protos::LastCommitInfo {
-            round: 0,
-            votes: Vec::new(),
-        };
+    pub fn begin_block(&mut self, chain_id: String, hash: Vec<u8>, proposer_address: Vec<u8>) -> AbciResult<()> {
+        let now = SystemTime::now();
 
         let request = tonic::Request::new(protos::RequestBeginBlock {
-            hash: hash,
-            header: None,
-            last_commit_info: Some(last_commit_info),
-            byzantine_validators: Vec::new(),
+            hash,
+            header: Some(protos::Header {
+                version: None,
+                chain_id,
+                height: 1,
+                time: Some(now.into()),
+                last_block_id: None,
+                last_commit_hash: vec![],
+                data_hash: vec![],
+                validators_hash: vec![],
+                next_validators_hash: vec![],
+                consensus_hash: vec![],
+                app_hash: vec![],
+                last_results_hash: vec![],
+                evidence_hash: vec![],
+                proposer_address,
+            }),
+            last_commit_info: None,
+            byzantine_validators: vec![],
         });
 
         let future = self.client.begin_block(request);
@@ -185,7 +212,7 @@ mod test {
     fn test_abci_deliver_tx() {
         let result = connect_or_get_connection(DEFAULT_ABCI_URL)
             .unwrap()
-            .deliver_tx(Vec::new());
+            .deliver_tx(vec![]);
         println!("deliver_tx result: {:?}", result);
         assert_eq!(result.is_ok(), true);
     }
@@ -194,17 +221,8 @@ mod test {
     fn test_abci_check_tx() {
         let result = connect_or_get_connection(DEFAULT_ABCI_URL)
             .unwrap()
-            .check_tx(Vec::new(), 0);
+            .check_tx(vec![], 0);
         println!("check_tx result: {:?}", result);
-        assert_eq!(result.is_ok(), true);
-    }
-
-    #[test]
-    fn test_abci_init_chain() {
-        let result = connect_or_get_connection(DEFAULT_ABCI_URL)
-            .unwrap()
-            .init_chain("chain_id".to_owned(), Vec::new());
-        println!("init_chain result: {:?}", result);
         assert_eq!(result.is_ok(), true);
     }
 
@@ -212,22 +230,187 @@ mod test {
     fn test_abci_begin_block() {
         let result = connect_or_get_connection(DEFAULT_ABCI_URL)
             .unwrap()
-            .begin_block(Vec::new());
+            .init_chain("test-chain-id".to_owned(), r#"{
+                "bank": {
+                    "params": {
+                        "default_send_enabled": true
+                    },
+                    "balances": [],
+                    "supply": [],
+                    "denom_metadata": [
+                        {
+                            "description": "The native staking token of the Cosmos Hub.",
+                            "denom_units": [
+                                {
+                                    "denom": "uatom",
+                                    "exponent": 0,
+                                    "aliases": [
+                                        "microatom"
+                                    ]
+                                },
+                                {
+                                    "denom": "matom",
+                                    "exponent": 3,
+                                    "aliases": [
+                                        "milliatom"
+                                    ]
+                                },
+                                {
+                                    "denom": "atom",
+                                    "exponent": 6
+                                }
+                            ],
+                            "base": "uatom",
+                            "display": "atom"
+                        }
+                    ]
+                },
+                "genutil": {
+                    "gentxs": []
+                },
+                "capability": {
+                    "index": "1",
+                    "owners": []
+                },
+                "mint": {
+                    "minter": {
+                        "inflation": "0.130000000000000000",
+                        "annual_provisions": "0.000000000000000000"
+                    },
+                    "params": {
+                        "mint_denom": "stake",
+                        "inflation_rate_change": "0.130000000000000000",
+                        "inflation_max": "0.200000000000000000",
+                        "inflation_min": "0.070000000000000000",
+                        "goal_bonded": "0.670000000000000000",
+                        "blocks_per_year": "6311520"
+                    }
+                },
+                "ibc": {
+                    "client_genesis": {
+                        "clients": [],
+                        "clients_consensus": [],
+                        "create_localhost": true
+                    },
+                    "connection_genesis": {
+                        "connections": [],
+                        "client_connection_paths": []
+                    },
+                    "channel_genesis": {
+                        "channels": [],
+                        "acknowledgements": [],
+                        "commitments": [],
+                        "send_sequences": [],
+                        "recv_sequences": [],
+                        "ack_sequences": []
+                    }
+                },
+                "upgrade": {},
+                "evidence": {
+                    "evidence": []
+                },
+                "auth": {
+                    "params": {
+                        "max_memo_characters": "256",
+                        "tx_sig_limit": "7",
+                        "tx_size_cost_per_byte": "10",
+                        "sig_verify_cost_ed25519": "590",
+                        "sig_verify_cost_secp256k1": "1000"
+                    },
+                    "accounts": []
+                },
+                "gov": {
+                    "starting_proposal_id": "1",
+                    "deposits": null,
+                    "votes": null,
+                    "proposals": null,
+                    "deposit_params": {
+                        "min_deposit": [
+                            {
+                                "denom": "stake",
+                                "amount": "10000000"
+                            }
+                        ],
+                        "max_deposit_period": "172800000000000"
+                    },
+                    "voting_params": {
+                        "voting_period": "172800000000000"
+                    },
+                    "tally_params": {
+                        "quorum": "0.334000000000000000",
+                        "threshold": "0.500000000000000000",
+                        "veto": "0.334000000000000000"
+                    }
+                },
+                "params": null,
+                "transfer": {
+                    "port_id": "transfer"
+                },
+                "crisis": {
+                    "constant_fee": {
+                        "denom": "stake",
+                        "amount": "1000"
+                    }
+                },
+                "distribution": {
+                    "params": {
+                        "community_tax": "0.020000000000000000",
+                        "base_proposer_reward": "0.010000000000000000",
+                        "bonus_proposer_reward": "0.040000000000000000",
+                        "withdraw_addr_enabled": true
+                    },
+                    "fee_pool": {
+                        "community_pool": []
+                    },
+                    "delegator_withdraw_infos": [],
+                    "previous_proposer": "",
+                    "outstanding_rewards": [],
+                    "validator_accumulated_commissions": [],
+                    "validator_historical_rewards": [],
+                    "validator_current_rewards": [],
+                    "delegator_starting_infos": [],
+                    "validator_slash_events": []
+                },
+                "slashing": {
+                    "params": {
+                        "signed_blocks_window": "100",
+                        "min_signed_per_window": "0.500000000000000000",
+                        "downtime_jail_duration": "600000000000",
+                        "slash_fraction_double_sign": "0.050000000000000000",
+                        "slash_fraction_downtime": "0.010000000000000000"
+                    },
+                    "signing_infos": {},
+                    "missed_blocks": {}
+                },
+                "staking": {
+                    "params": {
+                        "unbonding_time": "1814400000000000",
+                        "max_validators": 100,
+                        "max_entries": 7,
+                        "historical_entries": 100,
+                        "bond_denom": "stake"
+                    },
+                    "last_total_power": "0",
+                    "last_validator_powers": null,
+                    "validators": null,
+                    "delegations": null,
+                    "unbonding_delegations": null,
+                    "redelegations": null,
+                    "exported": false
+                }
+            }"#.as_bytes().to_vec());
+        println!("init_chain result: {:?}", result);
+        assert_eq!(result.is_ok(), true);
+        let result = connect_or_get_connection(DEFAULT_ABCI_URL)
+            .unwrap()
+            .begin_block("test-chain-id".to_owned(), vec![], "cosmos106vrzv5xkheqhjm023pxcxlqmcjvuhtfyachz4".as_bytes().to_vec());
         println!("begin_block result: {:?}", result);
         assert_eq!(result.is_ok(), true);
-    }
-
-    #[test]
-    fn test_abci_end_block() {
         let result = connect_or_get_connection(DEFAULT_ABCI_URL)
             .unwrap()
             .end_block(10);
         println!("end_block result: {:?}", result);
         assert_eq!(result.is_ok(), true);
-    }
-
-    #[test]
-    fn test_abci_commit() {
         let result = connect_or_get_connection(DEFAULT_ABCI_URL)
             .unwrap()
             .commit();
