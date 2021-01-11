6241e7be2c3f75eacf926f124cb007826ff50d25
Mr-Leshiy <leshiy12345678@gmail.com>
2021-01-11 15:02:04 +0200

    fix clippy

--- cosmos-abci/abci/build.rs
+++ cosmos-abci/abci/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     tonic_build::configure()
-        .type_attribute(".google.protobuf.Timestamp", "#[derive(serde::Serialize, serde::Deserialize)]")
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile(&["proto/types.proto"], &["proto"])
         .unwrap();
