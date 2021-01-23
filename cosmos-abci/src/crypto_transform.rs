2c47f0f80eb1995d07394f56610bfc829333f3fe
maryjanyes <maryjanyes@gmail.com>
2021-01-23 17:25:54 +0200

    PCI-233 Update tests.

--- cosmos-abci/src/crypto_transform.rs
+++ cosmos-abci/src/crypto_transform.rs
@@ -30,32 +30,35 @@ pub fn encode_value_from_base64(value: &[u8]) -> Vec<u8> {
     base64::decode(value).unwrap().to_vec()
 }
 
-#[test]
-fn should_get_address_from_ed25519_pub_key() {
-    const PUB_KEY: &str = "4MQ5aiE4zs1IqkLU3C0vWHUYhZcg40AX4k/wlsgcLCY=";
-    const ADDRESS: &str = "1AD4A13D8239FB9C6917DF0C52DACE3DC3D9C046";
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn should_get_address_from_ed25519_pub_key() {
+        const PUB_KEY: &str = "4MQ5aiE4zs1IqkLU3C0vWHUYhZcg40AX4k/wlsgcLCY=";
+        const ADDRESS: &str = "1AD4A13D8239FB9C6917DF0C52DACE3DC3D9C046";
 
-    let pub_key = base64::decode(PUB_KEY).unwrap();
-    let address = get_address_from_pub_key(
-        &pub_key,
-        PubKeyTypes::Ed25519,
-    );
-    let address_str = hex::encode(address).to_ascii_uppercase();
+        let pub_key = base64::decode(PUB_KEY).unwrap();
+        let address = crate::crypto_transform::get_address_from_pub_key(
+            &pub_key,
+            crate::crypto_transform::PubKeyTypes::Ed25519,
+        );
+        let address_str = hex::encode(address).to_ascii_uppercase();
 
-    assert_eq!(address_str, ADDRESS);
-}
+        assert_eq!(address_str, ADDRESS);
+    }
 
-#[test]
-fn should_get_address_from_secp256k1_pub_key() {
-    const PUB_KEY: &str = "4MQ5aiE4zs1IqkLU3C0vWHUYhZcg40AX4k/wlsgcLCY=";
-    const ADDRESS: &str = "QJBGWofcbE8e+PX5+PADgdxzOW8=";
+    #[test]
+    fn should_get_address_from_secp256k1_pub_key() {
+        const ADDRESS: &str = "2jLQaRKYMH6Sukx97FZRPqn494Ar";
+        const PUB_KEY: &str = "02950e1cdfcb133d6024109fd489f734eeb4502418e538c28481f22bce276f248c";
+    
+        let pub_key = hex::decode(PUB_KEY).unwrap();
+        let address = crate::crypto_transform::get_address_from_pub_key(
+            &pub_key,
+            crate::crypto_transform::PubKeyTypes::Secp256k1,
+        );
+        let address_str = bs58::encode(address).into_string();
 
-    let pub_key = base64::decode(PUB_KEY).unwrap();
-    let address = get_address_from_pub_key(
-        &pub_key,
-        PubKeyTypes::Secp256k1,
-    );
-    let address_str = base64::encode(address);
-
-    assert_eq!(address_str, ADDRESS);
+        assert_eq!(address_str, ADDRESS);
+    }
 }
