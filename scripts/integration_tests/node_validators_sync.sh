0fa4176071a7abc8c5cefd1846901921dd7ed11d
Mr-Leshiy <leshiy12345678@gmail.com>
2021-01-28 18:26:49 +0200

    fix

--- scripts/integration_tests/node_validators_sync.sh
+++ scripts/integration_tests/node_validators_sync.sh
@@ -7,6 +7,8 @@ expect_validators_set_1="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY@5FHneW
 expect_validators_set_2="5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty"
 expect_validators_set_3="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
 
+cosmos_validator_pub_key="0xa911e89ab8aec83f3c15701e1305486f47403a541659d20c3c43929cc31d34b9"
+
 source ./testing_setup/basic_setup.sh
 source ./testing_setup/test_utils.sh
 
@@ -19,13 +21,13 @@ cd ../../node_testing_ui
 
 validators_set=$(node ./get-validators.app.js)
 assert_eq "$validators_set" $expect_validators_set_1
-node ./insert-cosmos-validator.app.js //Bob 0 0xa911e89ab8aec83f3c15701e1305486f47403a541659d20c3c43929cc31d34b9
+node ./insert-cosmos-validator.app.js //Bob 0 $cosmos_validator_pub_key
 sleep 30s
 
 validators_set=$(node ./get-validators.app.js)
 assert_eq "$validators_set" $expect_validators_set_2
 
-node ./insert-cosmos-validator.app.js //Alice 0 0xa911e89ab8aec83f3c15701e1305486f47403a541659d20c3c43929cc31d34b9
+node ./insert-cosmos-validator.app.js //Alice 0 $cosmos_validator_pub_key
 sleep 30s
 
 validators_set=$(node ./get-validators.app.js)
